208f93471e52603a9297a1d61e498cee9f1fd781
gyouzazuoyg <gyouza_azuoyg@outlook.com>
2022-11-26 22:20:17 -0800

    Enable login as different types of users

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -5,8 +5,24 @@ const DB = initMockDB();
 
 export const handlers = [
   rest.post('/api/users/login', (req, res, ctx) => {
-    let retval = DB.users.dummyStudent;
+    let retval = {};
     let status = 200;
+    switch (req.body.username) {
+      case 'student':
+        retval = DB.users.dummyStudent;
+        break;
+      case 'admin':
+        retval = DB.users.dummyAdmin;
+        break;
+      case 'employer':
+        retval = DB.users.dummyEmployer;
+        break;
+      case 'guest':
+        retval = DB.users.dummyGuest;
+        break;
+      default:
+        retval = DB.users.dummyStudent;
+    }
 
     return res(
       // Respond with a 200 status code
