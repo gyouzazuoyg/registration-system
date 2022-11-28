69f1ca7b68017e1e7683d56ee4f8fac9b8c3e98d
gyouzazuoyg <gyouza_azuoyg@outlook.com>
2022-11-27 21:51:26 -0800

    Add temp /registercourse endpoint

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -58,4 +58,41 @@ export const handlers = [
       ctx.json(retval),
     );
   }),
+
+  rest.post('/api/courses/registercourse', (req, res, ctx) => {
+    const { user, course } = req.body;
+    const userId = user._id;
+    const courseId = course._id;
+    let courseDetails = {};
+    for (const key in DB.courses) {
+      if (DB.courses[key]._id === courseId) {
+        courseDetails = DB.courses[key];
+      }
+    }
+    let userDetails = {};
+    for (const key in DB.users) {
+      if (DB.users[key]._id === userId) {
+        userDetails = DB.users[key];
+      }
+    }
+
+    const registeredCandidate = {
+      userid: user._id,
+      registeredDate: moment().format('MMM DD yyyy'),
+    };
+    courseDetails.registeredCandidates.push(registeredCandidate);
+
+    const registeredCourse = {
+      courseid: course._id,
+      registeredDate: moment().format('MMM DD yyyy'),
+    };
+    userDetails.registeredCourses.push(registeredCourse);
+
+    let status = 200;
+    return res(
+      // Respond with a 200 status code
+      ctx.status(status),
+      ctx.text('Course Registered Successfully'),
+    );
+  }),
 ];
