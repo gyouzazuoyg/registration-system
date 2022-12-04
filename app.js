c17f38647a53818d9d8685f805031e56c1444831
gyouzazuoyg <gyouza_azuoyg@outlook.com>
2022-12-03 22:38:12 -0800

    Remove unused /api route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 var coursesRouter = require('./routes/courses');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', apiRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/courses', coursesRouter);
 
--- routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET api home page. */
-router.get('/', function (req, res, next) {
-  res.render('api', { title: 'Express API' });
-});
-
-module.exports = router;
